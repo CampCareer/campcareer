33a66d86d8fdc3564011bbe4ec0909ac60debfa9
Yaehun Lee <campcareer99@gmail.com>
2026-07-30 18:17:14 +0100

    refactor(db): reclassify occupation catalog source tables

---
+++ supabase/migrations/20260730181003_move_occupation_catalogs_to_ingest.sql
@@ -0,0 +1,6 @@
+alter table public.occupations_au set schema ingest;
+alter table public.occupations_ca set schema ingest;
+alter table public.occupations_uk set schema ingest;
+alter table public.occupation_state_au set schema ingest;
+alter table public.occupation_state_ca set schema ingest;
+alter table public.occupation_state_uk set schema ingest;
