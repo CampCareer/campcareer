25bafa6c20e07e23e03f58a4e71dc98210bdf560
Yaehun Lee <campcareer99@gmail.com>
2026-07-30 18:16:36 +0100

    refactor(db): move catalog source tables to ingest

---
+++ supabase/migrations/20260730181002_move_catalog_sources_to_ingest.sql
@@ -0,0 +1,24 @@
+alter table public.cities_au set schema ingest;
+alter table public.cities_ca set schema ingest;
+alter table public.cities_ie set schema ingest;
+alter table public.cities_uk set schema ingest;
+alter table public.cities_us set schema ingest;
+
+alter table public.colleges_au set schema ingest;
+alter table public.colleges_ca set schema ingest;
+alter table public.colleges_ie set schema ingest;
+alter table public.colleges_uk set schema ingest;
+alter table public.colleges_us set schema ingest;
+alter table public.colleges_nl set schema ingest;
+
+alter table public.courses_au set schema ingest;
+alter table public.courses_ca set schema ingest;
+alter table public.courses_ie set schema ingest;
+alter table public.courses_uk set schema ingest;
+alter table public.programs_us set schema ingest;
+
+alter table public.field_earnings_au set schema ingest;
+alter table public.field_earnings_ca set schema ingest;
+alter table public.field_earnings_ie set schema ingest;
+alter table public.field_earnings_uk set schema ingest;
+alter table public.graduate_outcomes_ie set schema ingest;
