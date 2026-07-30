0cd3380e0de8f0408f3d0760d9a77a64fac0ee5b
Yaehun Lee <campcareer99@gmail.com>
2026-07-30 18:39:59 +0100

    chore(db): move pending report retirement after applied cutover migrations

--- supabase/migrations/20260730181010_retire_report_workspace_tables.sql
+++
@@ -1,4 +0,0 @@
-alter table public.report_decision_options set schema retired;
-alter table public.report_intakes set schema retired;
-alter table public.report_launch_interests set schema retired;
-alter table public.report_orders set schema retired;
