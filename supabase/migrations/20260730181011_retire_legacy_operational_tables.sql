4a979513f9ce86acbf99919e924bc6b335d5cb4d
Yaehun Lee <campcareer99@gmail.com>
2026-07-30 18:24:37 +0100

    refactor(db): isolate retired operational tables

---
+++ supabase/migrations/20260730181011_retire_legacy_operational_tables.sql
@@ -0,0 +1,5 @@
+alter table public.programme_evidence set schema retired;
+alter table public.program_completions set schema retired;
+alter table public.contribution_submissions set schema retired;
+alter table public.reputation_ledger set schema retired;
+alter table public.notifications_sent set schema retired;
