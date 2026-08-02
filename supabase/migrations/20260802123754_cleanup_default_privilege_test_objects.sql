0a5955feebb357b3efbb3b5c31317a12fa2efe78
Yaehun Lee <leeyaehun@gmail.com>
2026-08-02 14:06:15 +0100

    chore(supabase): sync applied default privilege migrations

---
+++ supabase/migrations/20260802123754_cleanup_default_privilege_test_objects.sql
@@ -0,0 +1,5 @@
+DROP FUNCTION IF EXISTS public.__cc_acl_test_fn_20260802();
+DROP SEQUENCE IF EXISTS public.__cc_acl_test_seq_20260802;
+DROP TABLE IF EXISTS public.__cc_acl_test_20260802;
+DROP FUNCTION IF EXISTS api_private.__cc_acl_test_fn_20260802();
+DROP TABLE IF EXISTS api_private.__cc_acl_test_20260802;
