8b28d692b448df02ba93f3e79623f52479ebb2d6
Yaehun Lee <campcareer99@gmail.com>
2026-08-04 22:32:10 +0100

    refactor(methodology): lead with source register

--- src/app/methodology/layout.tsx
+++ src/app/methodology/layout.tsx
@@ -3,8 +3,8 @@ import { AustraliaCountrySources } from "./australia-country-sources"
 export default function MethodologyLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {children}
       <AustraliaCountrySources />
+      {children}
     </>
   )
 }
