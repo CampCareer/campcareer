efe12c4eef85fdf41872daf87333ec64d0b3b571
Yaehun Lee <campcareer99@gmail.com>
2026-08-08 23:26:55 +0100

    feat(institutions): load France explorer

--- src/lib/institutions/institutions.server.ts
+++ src/lib/institutions/institutions.server.ts
@@ -125,7 +125,9 @@ export async function searchInstitutions(
             ? "institution_explorer_sg_v1"
             : countryCode === "DE"
               ? "institution_explorer_de_v1"
-              : "institution_explorer_v1"
+              : countryCode === "FR"
+                ? "institution_explorer_fr_v1"
+                : "institution_explorer_v1"
 
   let query = supabaseAdmin
     .from(explorerView)
