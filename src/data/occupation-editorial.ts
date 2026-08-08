7a734550ee34394e82877119ffcea7fc12ec2936
Yaehun Lee <campcareer99@gmail.com>
2026-08-08 13:45:06 +0100

    Compose Network Administrator editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -1,11 +1,13 @@
 import { OCCUPATION_EDITORIAL as BASE_OCCUPATION_EDITORIAL } from "./occupation-editorial-base"
 import { TECHNOLOGY_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology"
+import { TECHNOLOGY_NETWORK_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-network"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
 export const OCCUPATION_EDITORIAL = [
   ...BASE_OCCUPATION_EDITORIAL,
   ...TECHNOLOGY_OCCUPATION_EDITORIAL,
+  ...TECHNOLOGY_NETWORK_OCCUPATION_EDITORIAL,
 ] as const
 
 const BY_ID = new Map(OCCUPATION_EDITORIAL.map((item) => [item.id, item]))
