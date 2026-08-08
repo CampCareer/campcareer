53b4e294fa1d6604184a661906afba015de04457
Yaehun Lee <campcareer99@gmail.com>
2026-08-08 22:50:52 +0100

    feat(institutions): load New Zealand explorer

--- src/lib/institutions/institutions.server.ts
+++ src/lib/institutions/institutions.server.ts
@@ -119,7 +119,9 @@ export async function searchInstitutions(
       ? "institution_explorer_ca_v1"
       : countryCode === "NL"
         ? "institution_explorer_nl_v1"
-        : "institution_explorer_v1"
+        : countryCode === "NZ"
+          ? "institution_explorer_nz_v1"
+          : "institution_explorer_v1"
 
   let query = supabaseAdmin
     .from(explorerView)
