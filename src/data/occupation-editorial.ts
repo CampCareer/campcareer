b78af3c8f5241841bf28112cdbf8eaa559bf783c
Yaehun Lee <campcareer99@gmail.com>
2026-08-08 15:29:29 +0100

    Wire Engineering occupation editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -4,6 +4,7 @@ import { TECHNOLOGY_NETWORK_OCCUPATION_EDITORIAL } from "./occupation-editorial-
 import { TECHNOLOGY_CLOUD_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-cloud"
 import { TECHNOLOGY_DATABASE_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-database"
 import { TECHNOLOGY_SUPPORT_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-support"
+import { ENGINEERING_OCCUPATION_EDITORIAL } from "./occupation-editorial-engineering"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
@@ -14,6 +15,7 @@ export const OCCUPATION_EDITORIAL = [
   ...TECHNOLOGY_CLOUD_OCCUPATION_EDITORIAL,
   ...TECHNOLOGY_DATABASE_OCCUPATION_EDITORIAL,
   ...TECHNOLOGY_SUPPORT_OCCUPATION_EDITORIAL,
+  ...ENGINEERING_OCCUPATION_EDITORIAL,
 ] as const
 
 const BY_ID = new Map(OCCUPATION_EDITORIAL.map((item) => [item.id, item]))
