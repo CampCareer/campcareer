3d23dd1f55e9103929add9fe7ec33c5497a9f690
Yaehun Lee <campcareer99@gmail.com>
2026-08-08 14:05:21 +0100

    Compose Database Administrator editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -2,6 +2,7 @@ import { OCCUPATION_EDITORIAL as BASE_OCCUPATION_EDITORIAL } from "./occupation-
 import { TECHNOLOGY_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology"
 import { TECHNOLOGY_NETWORK_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-network"
 import { TECHNOLOGY_CLOUD_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-cloud"
+import { TECHNOLOGY_DATABASE_OCCUPATION_EDITORIAL } from "./occupation-editorial-technology-database"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
@@ -10,6 +11,7 @@ export const OCCUPATION_EDITORIAL = [
   ...TECHNOLOGY_OCCUPATION_EDITORIAL,
   ...TECHNOLOGY_NETWORK_OCCUPATION_EDITORIAL,
   ...TECHNOLOGY_CLOUD_OCCUPATION_EDITORIAL,
+  ...TECHNOLOGY_DATABASE_OCCUPATION_EDITORIAL,
 ] as const
 
 const BY_ID = new Map(OCCUPATION_EDITORIAL.map((item) => [item.id, item]))
