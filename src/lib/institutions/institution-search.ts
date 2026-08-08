039ab19c89d149dbb046bfa593816086171fa616
Yaehun Lee <campcareer99@gmail.com>
2026-08-08 15:15:46 +0100

    Expose Ireland Institution routes

--- src/lib/institutions/institution-search.ts
+++ src/lib/institutions/institution-search.ts
@@ -1,4 +1,4 @@
-export const INSTITUTION_MVP_COUNTRIES = ["AU", "CA"] as const
+export const INSTITUTION_MVP_COUNTRIES = ["AU", "CA", "IE"] as const
 export type InstitutionMvpCountryCode = (typeof INSTITUTION_MVP_COUNTRIES)[number]
 
 export const INSTITUTION_KIND_VALUES = [
