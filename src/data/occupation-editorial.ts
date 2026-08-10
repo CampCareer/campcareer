eb2e3559e66bd54f0caa5292d5dc21104a3e66d6
Yaehun Lee <campcareer99@gmail.com>
2026-08-10 22:54:54 +0100

    Register Japan education editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -64,6 +64,7 @@ import { JAPAN_HEALTH_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editor
 import { JAPAN_TECHNOLOGY_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-technology"
 import { JAPAN_ENGINEERING_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-engineering"
 import { JAPAN_BUSINESS_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-business"
+import { JAPAN_EDUCATION_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-education"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
@@ -136,6 +137,7 @@ const COUNTRY_OCCUPATION_EDITORIAL_OVERRIDES = [
   ...JAPAN_TECHNOLOGY_OCCUPATION_EDITORIAL_OVERRIDES,
   ...JAPAN_ENGINEERING_OCCUPATION_EDITORIAL_OVERRIDES,
   ...JAPAN_BUSINESS_OCCUPATION_EDITORIAL_OVERRIDES,
+  ...JAPAN_EDUCATION_OCCUPATION_EDITORIAL_OVERRIDES,
 ] as const
 
 export const OCCUPATION_EDITORIAL: readonly OccupationEditorialType[] = RAW_OCCUPATION_EDITORIAL.map((item) => {
