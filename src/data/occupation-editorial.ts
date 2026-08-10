0e3bab2b9e1c165414437ded6d8640633991a33a
Yaehun Lee <campcareer99@gmail.com>
2026-08-10 22:57:20 +0100

    Register Ireland technology occupation editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -61,6 +61,7 @@ import { AUTOMOTIVE_SERVICE_TECHNICIAN_OCCUPATION_EDITORIAL } from "./occupation
 import { CANADA_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-ca-carpenter"
 import { IE_CONSTRUCTION_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-ie-construction"
 import { IE_HEALTH_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-ie-health"
+import { IE_TECHNOLOGY_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-ie-technology"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
@@ -130,6 +131,7 @@ const COUNTRY_OCCUPATION_EDITORIAL_OVERRIDES = [
   ...CANADA_OCCUPATION_EDITORIAL_OVERRIDES,
   ...IE_CONSTRUCTION_OCCUPATION_EDITORIAL_OVERRIDES,
   ...IE_HEALTH_OCCUPATION_EDITORIAL_OVERRIDES,
+  ...IE_TECHNOLOGY_OCCUPATION_EDITORIAL_OVERRIDES,
 ] as const
 
 export const OCCUPATION_EDITORIAL: readonly OccupationEditorialType[] = RAW_OCCUPATION_EDITORIAL.map((item) => {
