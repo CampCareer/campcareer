442b8912bdcb006eee2827512e574e04d7b4f0d8
Yaehun Lee <campcareer99@gmail.com>
2026-08-11 22:07:08 +0100

    Wire Singapore hospitality occupation editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -67,6 +67,7 @@ import { SINGAPORE_BUSINESS_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-
 import { SINGAPORE_EDUCATION_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-sg-education"
 import { SINGAPORE_ENVIRONMENT_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-sg-environment"
 import { SINGAPORE_DESIGN_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-sg-design"
+import { SINGAPORE_HOSPITALITY_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-sg-hospitality"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
@@ -142,6 +143,7 @@ const COUNTRY_OCCUPATION_EDITORIAL_OVERRIDES = [
   ...SINGAPORE_EDUCATION_OCCUPATION_EDITORIAL_OVERRIDES,
   ...SINGAPORE_ENVIRONMENT_OCCUPATION_EDITORIAL_OVERRIDES,
   ...SINGAPORE_DESIGN_OCCUPATION_EDITORIAL_OVERRIDES,
+  ...SINGAPORE_HOSPITALITY_OCCUPATION_EDITORIAL_OVERRIDES,
 ] as const
 
 export const OCCUPATION_EDITORIAL: readonly OccupationEditorialType[] = RAW_OCCUPATION_EDITORIAL.map((item) => {
