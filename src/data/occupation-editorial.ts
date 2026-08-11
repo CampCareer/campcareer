83562f8ee7cb6d1c2b0b175890ddff625a93528c
Yaehun Lee <campcareer99@gmail.com>
2026-08-11 11:18:53 +0100

    Register Japan transport occupation editorial

--- src/data/occupation-editorial.ts
+++ src/data/occupation-editorial.ts
@@ -68,6 +68,7 @@ import { JAPAN_EDUCATION_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-edi
 import { JAPAN_ENVIRONMENT_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-environment"
 import { JAPAN_DESIGN_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-design"
 import { JAPAN_HOSPITALITY_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-hospitality"
+import { JAPAN_TRANSPORT_OCCUPATION_EDITORIAL_OVERRIDES } from "./occupation-editorial-jp-transport"
 
 export type { CountryOccupationEditorial, OccupationEditorial } from "./occupation-editorial-base"
 
@@ -144,6 +145,7 @@ const COUNTRY_OCCUPATION_EDITORIAL_OVERRIDES = [
   ...JAPAN_ENVIRONMENT_OCCUPATION_EDITORIAL_OVERRIDES,
   ...JAPAN_DESIGN_OCCUPATION_EDITORIAL_OVERRIDES,
   ...JAPAN_HOSPITALITY_OCCUPATION_EDITORIAL_OVERRIDES,
+  ...JAPAN_TRANSPORT_OCCUPATION_EDITORIAL_OVERRIDES,
 ] as const
 
 export const OCCUPATION_EDITORIAL: readonly OccupationEditorialType[] = RAW_OCCUPATION_EDITORIAL.map((item) => {
