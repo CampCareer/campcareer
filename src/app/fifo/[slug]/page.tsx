a01e93ab14766417e976105d06e94a1d349d3478
Yaehun Lee <campcareer99@gmail.com>
2026-08-16 02:25:59 +0100

    Route Scaffolder to verified detail

--- src/app/fifo/[slug]/page.tsx
+++ src/app/fifo/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { ALL_FIFO_PATHS, getAllFifoPath } from "@/lib/fifo/all-fifo-paths"
 import { FifoJobDetail } from "./fifo-job-detail"
 import { VerifiedEquipmentJobDetail } from "./verified-equipment-job-detail"
 import { VerifiedFifoJobDetail } from "./verified-fifo-job-detail"
+import { VerifiedScaffolderJobDetail } from "./verified-scaffolder-job-detail"
 
 type PageProps = {
   params: Promise<{ slug: string }>
@@ -45,6 +46,9 @@ export default async function FifoJobPage({ params }: PageProps) {
   if (path.status === "verified" && path.published) {
     if (path.slug === "dump-truck-operator") {
       return <VerifiedEquipmentJobDetail path={path} />
+    }
+    if (path.slug === "scaffolder") {
+      return <VerifiedScaffolderJobDetail path={path} />
     }
     return <VerifiedFifoJobDetail path={path} />
   }
