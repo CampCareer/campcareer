93e880ed55e5e20af11963561a43b960894e4e79
Yaehun Lee <campcareer99@gmail.com>
2026-08-16 02:25:09 +0100

    Promote evidence-backed Scaffolder path

--- src/lib/fifo/all-fifo-paths.ts
+++ src/lib/fifo/all-fifo-paths.ts
@@ -1,10 +1,12 @@
 import { FIFO_PATHS, type FifoPath } from "./fifo-paths"
 import { PLANT_EQUIPMENT_PATHS } from "./plant-equipment-paths"
+import { SCAFFOLDER_PATH } from "./scaffolder-path"
 
 export const ALL_FIFO_PATHS: readonly FifoPath[] = [
   ...FIFO_PATHS.filter((path) => path.slug === "drillers-offsider"),
   ...PLANT_EQUIPMENT_PATHS,
-  ...FIFO_PATHS.filter((path) => path.slug !== "drillers-offsider"),
+  ...FIFO_PATHS.filter((path) => path.slug === "plant-operator"),
+  SCAFFOLDER_PATH,
 ]
 
 export const HOME_FIFO_PATHS = ALL_FIFO_PATHS.filter((path) =>
